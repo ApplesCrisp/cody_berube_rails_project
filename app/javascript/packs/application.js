06c04c0f5cea231a23b7627d10228bc373f912ff
Cody Berube <cberube21@outlook.com>
2022-07-07 21:08:12 -0400

    removed turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,5 @@ import 'bootstrap';
 import '../stylesheets/application'
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
