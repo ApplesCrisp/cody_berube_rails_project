f0c3a6fca5b776e57ef54abee207487de01ca8b1
Cody Berube <cberube21@outlook.com>
2022-07-08 20:38:12 -0400

    Working on unit tests

--- app/models/album_artist.rb
+++
@@ -1,2 +0,0 @@
-class AlbumArtist < ApplicationRecord
-end
--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,6 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
+  test 'Should add artist to database' do
+    assert true
+  end
   # test "the truth" do
   #   assert true
   # end
